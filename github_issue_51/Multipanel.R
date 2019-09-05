8c16a1cb5a426ad83b60cec807b585386189740d
Bernat Gel <bernatgel@gmail.com>
2019-09-05 14:22:29 +0200

    Added low-res version of the image

--- github_issue_51/Multipanel.R
+++ github_issue_51/Multipanel.R
@@ -11,7 +11,7 @@ library(cowplot)
   p4 <- as.ggplot(expression(plotKaryotype(genome = "dm6", main="Fruit fly")))
   save_plot("Multipanel_cowplot.png", plot_grid(p1, p2, p3, p4, ncol=2, labels=LETTERS[1:9]), base_height=10)
 
-
+##https://user-images.githubusercontent.com/919489/64335084-59ec7680-cfda-11e9-9976-53416bf89ed8.png
 
 library(grid)
   p1 <- as.grob(expression(kp <- plotKaryotype(main="Human (hg19)", plot.params = pp, cex=1.5), kpDataBackground(kp), kpAbline(kp, h=0.5, col="red")))
