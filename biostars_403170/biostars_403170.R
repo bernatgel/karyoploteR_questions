c2c1ded2eccbb61a1f11c3c136980103b614e0d6
Bernat Gel <bernatgel@gmail.com>
2019-12-10 16:09:11 +0100

    Added comments

--- biostars_403170/biostars_403170.R
+++ biostars_403170/biostars_403170.R
@@ -1,14 +1,13 @@
 library(karyoploteR)
-
 library(Biostrings)
 
-
+#Read the fasta
 seqs <- Biostrings::readDNAStringSet("seqs.fa")
 
-#read the beds
+#read the ranges
 regs <- list(Bed1=toGRanges("Bed1.txt"), Bed2=toGRanges("Bed2.txt"))
 
-
+#Define the plotting parameters (margins, etc...)
 pp <- getDefaultPlotParams(1)
 pp$ideogramheight <- 0
 pp$data1inmargin <- 0
@@ -22,16 +21,18 @@ for(i in seq_along(seqs)) {
   seq.chars <- strsplit(x=as.character(s), "")[[1]]
 
   png(paste0(seq.name, ".png"), width=10*seq.len, height=300)
+    #Plot with a custom genome
     custom.genome <- toGRanges("Cust", 1, seq.len)
     kp <- plotKaryotype(genome=custom.genome, ideogram.plotter = NULL, labels.plotter = NULL, plot.params = pp)
     kpAddBaseNumbers(kp, tick.dist = 50, minor.ticks = FALSE, cex = 1)
     
+    #Add the sequence
     kpAddLabels(kp, labels = seq.name, r0 = 0, r1=0.1, cex=1.8)
     for(j in seq_len(seq.len)) {
       kpText(kp, chr="Cust", x = j, y=0.05, labels = seq.chars[j])
     }
     
-    #Plot the regs
+    #Add the ranges
     for(j in seq_along(regs)) {
       at <- autotrack(j, length(regs), r0=0.2)
       kpAddLabels(kp, labels = names(regs)[j], r0=at, cex=1.4)
