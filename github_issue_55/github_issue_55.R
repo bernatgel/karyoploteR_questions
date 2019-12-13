807855ef2adfc0e7986ff2ac509efa6d46e882db
Bernat Gel <bernatgel@gmail.com>
2019-12-13 15:27:31 +0100

    Added github issue 55

---
+++ github_issue_55/github_issue_55.R
@@ -0,0 +1,21 @@
+library(karyoploteR)
+
+
+cn.regs <- sort(createRandomRegions(nregions = 100))
+
+cyto <- getCytobands(genome="hg19")
+
+#remove the unneded cytoband names 
+cyto$name[!overlapsAny(cyto, cn.regs)] <- ""
+
+png(file="selected.cytobands.png", width=1000, height=300)
+  kp <- plotKaryotype(chromosomes = c("chr15", "chr16", "chr17"))
+  kpPlotRegions(kp, cn.regs)
+  kpText(kp, data=cyto, y=0.5, labels = cyto$name, data.panel = "ideogram", cex=0.8)
+dev.off()
+
+png(file="selected.cytobands.out.of.ideogram.png", width=1000, height=300)
+  kp <- plotKaryotype(chromosomes = c("chr15", "chr16", "chr17"))
+  kpPlotRegions(kp, cn.regs)
+  kpText(kp, data=cyto, y=0, labels = cyto$name, data.panel = "ideogram", cex=0.8, srt=45, pos=1)
+dev.off()
